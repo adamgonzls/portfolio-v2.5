ab6245d6d53163fdf5a3ecbb4cbc4ad2fb8395b4
Adam Gonzales <adamgonzales1@gmail.com>
2022-06-02 09:44:14 -0700

    feature: add ability to request external blog data and render on home page.

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,19 @@ import Intro from '../components/Intro'
 import Services from '../components/Services'
 import Work from '../components/Work'
 import About from '../components/About'
+import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +25,20 @@ export default function Home() {
       <Intro />
       <Services />
       <Work />
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
       <About />
     </Layout>
   )
